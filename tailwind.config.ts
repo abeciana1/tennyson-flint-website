de3c1819177646b6136334f3c51249e74477311d
abeciana1 <alex.beciana@gmail.com>
2024-12-10 18:54:17 -0700

    Added brand colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,13 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        white: '#EFF1F3',
+        black: '#1B1C22',
+        navy: '#173B54',
+        gold: '#E1C340',
+        violet: '#210D41',
+        red: '#DA2C38',
+        pink: '#CA2E55'
       },
     },
   },
