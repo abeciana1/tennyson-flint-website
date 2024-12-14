34595cd5a57781ca4a81e8d94a88e9786369a515
abeciana1 <alex.beciana@gmail.com>
2024-12-14 16:40:37 -0700

    Added canonical urls to page structures

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next'
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../components/builder";
 
@@ -7,6 +8,55 @@ interface PageProps {
   params: Promise<{
     page: string[];
   }>;
+}
+
+export const generateMetadata = async ({ params }: PageProps): Promise<Metadata> => {
+  const urlStructure: { page: string[]; } = (await params)
+  console.log('urlStructure', urlStructure)
+  const nestedRoute = urlStructure?.page?.length > 1
+  const slugParent = urlStructure?.page[0]
+  let modelName = 'page'
+  
+  if (nestedRoute) {
+    if (slugParent === 'blog') modelName = 'blog-post'
+    if (slugParent === 'book') modelName = 'book-page'
+  }
+  
+  const content = await builder
+    .get(modelName, {
+      userAttributes: {
+        urlPath: "/" + ((await params)?.page?.join("/") || ""),
+      },
+    })
+    .toPromise();
+
+  console.log('page content data', content)
+
+  if (modelName === 'blog-post') {
+    return {
+      title: content?.data?.title,
+      description: content?.data?.description,
+      alternates: {
+        canonical: `/blog/${urlStructure?.page[1]}`
+      }
+    }
+  } else if (modelName === 'book-page') {
+    return {
+      title: content?.data?.title,
+      description: content?.data?.description,
+      alternates: {
+        canonical: `/book/${urlStructure?.page[1]}`
+      }
+    }
+  } else {
+    return {
+      title: content?.data?.title,
+      description: content?.data?.description,
+      alternates: {
+        canonical: `/${urlStructure?.page[1]}`
+      }
+    }
+  }
 }
 
 export default async function Page(props: PageProps) {
