1bd04984b212e532571a800a5b5160418147e2c4
abeciana1 <alex.beciana@gmail.com>
2024-12-15 15:28:38 -0700

    Created Heading1 component

---
+++ components/_styled/headings/index.tsx
@@ -0,0 +1,17 @@
+import { HeadingsI } from '@/definitions/interfaces/_styled/headings'
+import cx from 'classnames'
+
+export const Heading1: React.FC<HeadingsI> = ({
+  text,
+  italic = false,
+  bold = false,
+}) => {
+  return (
+    <h1
+      className={cx('text-5xl font-cinzel-dec', {
+        ['italic']: italic,
+        ['font-bold']: bold,
+      })}
+    >{ text }</h1>
+  )
+}
