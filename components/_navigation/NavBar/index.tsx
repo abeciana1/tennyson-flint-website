b20aeef83b16ac507206b22850d695ea394f10e3
abeciana1 <alex.beciana@gmail.com>
2024-12-15 17:09:59 -0700

    Added padding and fetch for nav link data

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -1,10 +1,15 @@
 import { Heading1 } from '@/components/_styled/headings'
+import { builder } from "@builder.io/sdk";
 
-const NavBar = () => {
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+const NavBar = async () => {
+  const navLinks = await builder.getAll('navigation-links')
+  console.log('navLinks', navLinks)
   return (
     <header>
-      <nav className='flex flex-row'>
-        <Heading1 text='Tennyson Flint' />
+      <nav className='flex flex-row px-10 py-3'>
+        <Heading1 text='Tennyson Flint' fontSize='3xl' />
       </nav>
     </header>
   )
