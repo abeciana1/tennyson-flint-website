6bc92f3bcde6d1fbefba0deb98fb2718b7b81e67
abeciana1 <alex.beciana@gmail.com>
2024-12-15 17:10:30 -0700

    Added case font size rendering

--- components/_styled/headings/index.tsx
+++ components/_styled/headings/index.tsx
@@ -5,12 +5,15 @@ export const Heading1: React.FC<HeadingI> = ({
   text,
   italic = false,
   bold = false,
+  fontSize = '5xl'
 }) => {
   return (
     <h1
-      className={cx('text-5xl font-cinzel-dec', {
+      className={cx('font-cinzel-dec', {
         ['italic']: italic,
         ['font-bold']: bold,
+        ['text-5xl']: fontSize === '5xl',
+        ['text-3xl']: fontSize === '3xl'
       })}
     >{ text }</h1>
   )
