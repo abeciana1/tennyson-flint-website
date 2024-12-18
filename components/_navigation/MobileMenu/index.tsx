0a5f48ca9ff6ad4deb8d96385e6122c379a5a118
abeciana1 <alex.beciana@gmail.com>
2024-12-17 18:36:31 -0700

    Created MobileMenu component

---
+++ components/_navigation/MobileMenu/index.tsx
@@ -0,0 +1,60 @@
+'use client'
+import { useState, useEffect } from 'react'
+import NavLink from '@/components/_navigation/NavLink'
+import Hamburger from 'hamburger-react'
+import { fetchNavLinks } from '@/helper-functions/builder-fetch'
+import { BuilderContent } from '@builder.io/sdk';
+
+const MobileMenu = () => {
+  const [isOpen, setOpen] = useState(false)
+  const [navLinks, setNavLinks] = useState<BuilderContent[]>([]);
+  useEffect(() => {
+    const fetchLinks = async () => {
+      try {
+        const links = await fetchNavLinks();
+        setNavLinks(links);
+      } catch (error) {
+        console.error('Error fetching navigation links:', error);
+      }
+    }
+    fetchLinks()
+  }, [isOpen])
+
+  useEffect(() => {
+    const main = document?.getElementsByTagName('main')
+    if (isOpen && main) {
+      main[0].style.display = 'none';
+    } else {
+      main[0].style.display = 'block';
+    }
+  }, [isOpen])
+
+  return (
+    <>
+      <div className='block md:hidden'>
+        <Hamburger
+          direction="right"
+          toggled={isOpen}
+          toggle={setOpen}
+          aria-label={isOpen ? 'Close menu' : 'Open menu'}
+          aria-expanded={isOpen}
+        />
+      </div>
+      {isOpen &&
+        <ul className='md:hidden flex flex-col gap-6 w-full bg-white absolute top-16 left-0 pl-10 pt-4 h-screen z-50'>
+          {navLinks && navLinks?.map((link) => {
+            return (
+              <NavLink
+                key={link?.id}
+                label={link?.data?.linkText}
+                href={link?.data?.href}
+              />
+            )
+          })}
+        </ul>
+      }
+    </>
+  )
+}
+
+export default MobileMenu
