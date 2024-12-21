9c71e096ec3b8580627f6d3af710a9e4c2471120
abeciana1 <alex.beciana@gmail.com>
2024-12-21 11:54:46 -0600

    Deconstructed props and added rendering

---
+++ components/_styled/ImageComp/index.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image'
+import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
+import cx from 'classnames'
+import { ROUNDED } from '@/definitions/enums'
+
+const ImageComp: React.FC<ImageI> = ({
+  src,
+  alt,
+  width,
+  height,
+  rounded
+}) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      className={cx({
+        ['rounded-none']: ROUNDED.NONE,
+        ['rounded-sm']: rounded === ROUNDED.SM,
+        ['rounded-md']: rounded === ROUNDED.MD,
+        ['rounded-lg']: rounded === ROUNDED.LG,
+        ['rounded-xl']: rounded === ROUNDED.XL,
+        ['rounded-2xl']: rounded === ROUNDED.XXL,
+      })}
+    />
+  )
+}
