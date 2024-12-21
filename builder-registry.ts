77122fc2891b72adfc2cf3ef2c7f0fbb82f081d5
abeciana1 <alex.beciana@gmail.com>
2024-12-21 12:05:48 -0600

    Registered ImageComp component

--- builder-registry.ts
+++ builder-registry.ts
@@ -1,14 +1,33 @@
 "use client";
 import { builder, Builder } from "@builder.io/react";
+import ImageComp from '@/components/_styled/ImageComp'
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
-// Builder.registerComponent(Counter, {
-//   name: "Counter",
-//   inputs: [
-//     {
-//       name: "initialCount",
-//       type: "number",
-//     },
-//   ],
-// });
+Builder.registerComponent(ImageComp, {
+  name: "ImageComp",
+  inputs: [
+    {
+      name: "src",
+      type: "string",
+    },
+    {
+      name: "alt",
+      type: "string",
+    },
+    {
+      name: "width",
+      type: "number",
+    },
+    {
+      name: "height",
+      type: "number",
+    },
+    {
+      name: "rounded",
+      type: "enum",
+      options: ["NONE", "SM", "MD", "LG", "XL", "FULL", "XXL"],
+      helperText: "Choose between: NONE, SM, MD, LG, XL, FULL, or  XXL."
+    },
+  ],
+})
