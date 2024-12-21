49fca3715606ba13ddef174bf2eea881d535637f
abeciana1 <alex.beciana@gmail.com>
2024-12-21 13:08:59 -0600

    Created initial render of HeroSection component

---
+++ components/_sections/HeroSection/index.tsx
@@ -0,0 +1,11 @@
+import { Heading1, Heading2 } from '@/components/_styled/headings'
+
+const HeroSection: React.FC = () => {
+  return (
+    <section className='flex'>
+      
+    </section>
+  )
+}
+
+export default HeroSection
