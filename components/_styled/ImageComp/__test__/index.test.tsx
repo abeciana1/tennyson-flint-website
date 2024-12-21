9f3747a18c0287c9bd8bd7d27ffe9b8420197cb7
abeciana1 <alex.beciana@gmail.com>
2024-12-21 11:48:44 -0600

    Created test dir and root file for ImageComp component && Added initial failing test

---
+++ components/_styled/ImageComp/__test__/index.test.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react'
+import ImageComp from '@/components/_styled/ImageComp'
+import { ROUNDED } from '@/definitions/enums'
+
+describe('ImageComp', () => {
+  beforeEach(() => {
+    render(
+      <ImageComp
+        src='test.jpg'
+        alt='Test Image'
+        width={100}
+        height={100}
+        rounded={ROUNDED.MD}
+      />
+    )
+  })
+  test('should render with role of image', () => {
+    const img = screen.getByRole('img')
+    expect(img).toBeInTheDocument()
+  })
+})
