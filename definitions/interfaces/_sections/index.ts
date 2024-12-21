b5d5512ff2fb04854c2a468c35f5c37b9da8d6cc
abeciana1 <alex.beciana@gmail.com>
2024-12-21 13:41:05 -0600

    Made subheading prop optional

--- definitions/interfaces/_sections/index.ts
+++ definitions/interfaces/_sections/index.ts
@@ -2,7 +2,7 @@ import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
 
 export interface HeroSectionI {
   heading: string;
-  subheading: string;
+  subheading?: string;
   textBody: string;
   ctaChildren: React.ReactNode | React.ReactNode[];
   image: ImageI;
