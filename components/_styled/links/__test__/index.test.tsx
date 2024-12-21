cfd16bc3facdda110421922882d5de9f432b1db3
abeciana1 <alex.beciana@gmail.com>
2024-12-21 15:22:34 -0600

    Created tests for ButtonLink

---
+++ components/_styled/links/__test__/index.test.tsx
@@ -0,0 +1,32 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import ButtonLink from '@/components/_styled/links'
+import { COLORS, ROUNDED } from '@/definitions/enums'
+
+describe('Links', () => {
+  describe('ButtonLink', () => {
+    beforeEach(() => {
+      render(
+        <ButtonLink
+          linkText='Link'
+          href='/books'
+          targetBlank
+          arrow
+          title='Link to Books page'
+          bgColor={COLORS.NAVY}
+          rounded={ROUNDED.MD}
+        />
+      )
+    })
+    test('should render link with text', () => {
+      const link = screen.getByRole('link')
+      expect(link.textContent).toBe('Link')
+      expect(link).toBeInTheDocument()
+    })
+    test('should render link with href and title attributes', () => {
+      const link = screen.getByRole('link')
+      expect(link).toHaveAttribute('href', '/books')
+      expect(link).toHaveAttribute('title', 'Link to Books page')
+    })
+  })
+})
