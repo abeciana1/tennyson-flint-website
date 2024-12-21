22d912e085363c4147821576ba4acfd59a6ed144
abeciana1 <alex.beciana@gmail.com>
2024-12-21 12:07:23 -0600

    Created Heading2 component

--- components/_styled/headings/index.tsx
+++ components/_styled/headings/index.tsx
@@ -18,3 +18,20 @@ export const Heading1: React.FC<HeadingI> = ({
     >{ text }</h1>
   )
 }
+
+export const Heading2: React.FC<HeadingI> = ({
+  text,
+  italic = false,
+  bold = false,
+  fontSize = '4xl'
+}) => {
+  return (
+    <h1
+      className={cx('font-cinzel-dec', {
+        ['italic']: italic,
+        ['font-bold']: bold,
+        ['text-4xl']: fontSize === '4xl',
+      })}
+    >{ text }</h1>
+  )
+}
