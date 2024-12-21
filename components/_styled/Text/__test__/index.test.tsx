e76c6db22df106755fa86d063e5e2545d4fa4f37
abeciana1 <alex.beciana@gmail.com>
2024-12-21 13:39:18 -0600

    Created initial testing for TextContent component

---
+++ components/_styled/Text/__test__/index.test.tsx
@@ -0,0 +1,18 @@
+import '@testing-library/jest-dom';
+import { render, screen }  from '@testing-library/react'
+import TextContent from '@/components/_styled/Text'
+
+
+describe('TextContent', () => {
+  beforeEach(() => {
+    render(
+      <TextContent
+        text='Hello World!'
+      />
+    )
+  })
+  test('should render text content', () => {
+    const text = screen.getByText('Hello World!')
+    expect(text).toBeInTheDocument()
+  })
+})
