ca18f022756e02f09ec453f90e2f736aab88a00e
abeciana1 <alex.beciana@gmail.com>
2024-12-21 15:20:56 -0600

    Created BasicLinkI and ButtonLinkI interfaces

---
+++ definitions/interfaces/_styled/Links/index.ts
@@ -0,0 +1,14 @@
+import { COLORS, ROUNDED } from '@/definitions/enums'
+
+export interface BasicLinkI {
+  linkText: string;
+  title: string;
+  href: string;
+  targetBlank: boolean;
+}
+
+export interface ButtonLinkI extends BasicLinkI {
+  bgColor: COLORS;
+  arrow?: boolean;
+  rounded: ROUNDED;
+}
