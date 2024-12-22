0085140fa7aa207f51f2341fcbd8269cfaf75f6d
abeciana1 <alex.beciana@gmail.com>
2024-12-22 09:19:48 -0600

    Added MarginSection and wrapped content

--- components/_sections/HeroSection/index.tsx
+++ components/_sections/HeroSection/index.tsx
@@ -6,6 +6,7 @@ import { FONT_SIZE } from '@/definitions/enums'
 import { ButtonLink } from '@/components/_styled/links'
 import { ButtonLinkI } from '@/definitions/interfaces/_styled/Links'
 import { COLORS, ROUNDED } from '@/definitions/enums'
+import MarginSection from '@/components/_sections/MarginSection'
 
 const HeroSection: React.FC<HeroSectionI> = ({
   preHeading = '',
@@ -24,62 +25,64 @@ const HeroSection: React.FC<HeroSectionI> = ({
   } = image
   console.log('ctaChildren', ctaChildren)
   return (
-    <section className='flex items-center'>
-      <section>
-        {preHeading &&
+    <MarginSection>
+      <section className='flex items-center'>
+        <section>
+          {preHeading &&
+            <TextContent
+              text={preHeading}
+              fontSize={FONT_SIZE.XXL}
+            />
+          }
+          <Heading1
+            text={heading}
+            bold
+          />
+          {subheading &&
+            <Heading2
+              text={subheading}
+              semiBold
+            />
+          }
           <TextContent
-            text={preHeading}
-            fontSize={FONT_SIZE.XXL}
+            text={textBody}
           />
-        }
-        <Heading1
-          text={heading}
-          bold
-        />
-        {subheading &&
-          <Heading2
-            text={subheading}
-            semiBold
+          {ctaChildren && 
+            <div className='flex flex-wrap gap-5'>
+              {ctaChildren?.map(({
+                linkText,
+                title,
+                href,
+                targetBlank,
+                bgColor,
+                rounded,
+                arrow
+              }: ButtonLinkI) => (
+                <ButtonLink
+                  key={linkText}
+                  linkText={linkText}
+                  title={title}
+                  href={href}
+                  targetBlank={targetBlank}
+                  bgColor={COLORS[bgColor]}
+                  rounded={ROUNDED[rounded]}
+                  arrow={arrow}
+                />
+              ))}
+            </div>
+          }
+        </section>
+        <section>
+          <ImageComp
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+            rounded={rounded}
           />
-        }
-        <TextContent
-          text={textBody}
-        />
-        {ctaChildren && 
-          <div className='flex flex-wrap gap-5'>
-            {ctaChildren?.map(({
-              linkText,
-              title,
-              href,
-              targetBlank,
-              bgColor,
-              rounded,
-              arrow
-            }: ButtonLinkI) => (
-              <ButtonLink
-                key={linkText}
-                linkText={linkText}
-                title={title}
-                href={href}
-                targetBlank={targetBlank}
-                bgColor={COLORS[bgColor]}
-                rounded={ROUNDED[rounded]}
-                arrow={arrow}
-              />
-            ))}
-          </div>
-        }
+        </section>
       </section>
-      <section>
-        <ImageComp
-          src={src}
-          alt={alt}
-          width={width}
-          height={height}
-          rounded={rounded}
-        />
-      </section>
-    </section>
+    </MarginSection>
   )
 }
 
