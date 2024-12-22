f696582980a177ff0ba447f5e940e44f42bc99f8
abeciana1 <alex.beciana@gmail.com>
2024-12-22 10:17:13 -0600

    Added nextjs config for  Builder images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import BuilderDevTools from "@builder.io/dev-tools/next";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = BuilderDevTools()({
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.builder.io',
+      }
+    ]
+  }
 });
 
 export default nextConfig;
