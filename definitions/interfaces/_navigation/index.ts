cc0cbf835fe488297c4aa507b834a91e6e7eabef
abeciana1 <alex.beciana@gmail.com>
2024-12-23 16:12:31 -0600

    Created FooterI interface

--- definitions/interfaces/_navigation/index.ts
+++ definitions/interfaces/_navigation/index.ts
@@ -1,4 +1,11 @@
+import { SocialLinkI } from '@/definitions/interfaces/_styled/Links'
+
 export interface NavLinkI {
   label: string;
   href: string;
+}
+
+export interface FooterI {
+  siteDescription: string;
+  socialLinks: SocialLinkI[];
 }
