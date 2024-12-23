0aaf4a74a1bea2d44e91d47b545820f60f5b3373
abeciana1 <alex.beciana@gmail.com>
2024-12-23 16:13:44 -0600

    Created index page route

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from '@/components/builder'
+
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+
+interface PageProps {
+  params: Promise<{
+    page: string[];
+  }>;
+}
+
+export default async function Page(props: PageProps) {
+  const builderModelName = "page";
+
+  const content = await builder
+    .get(builderModelName, {
+      userAttributes: {
+        urlPath: "/" + ((await props?.params)?.page?.join("/") || ""),
+      },
+    })
+    .toPromise();
+  return (
+    <>
+      <div>
+        <main>
+          <RenderBuilderContent content={content} model={builderModelName} />
+        </main>
+      </div>
+    </>
+  );
+}
