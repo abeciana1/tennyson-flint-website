fe3b29a63708ae90ac3550d38a642e87e04b6529
abeciana1 <alex.beciana@gmail.com>
2024-12-23 16:15:56 -0600

    Added props and rendering for fields

--- components/_navigation/Footer/index.tsx
+++ components/_navigation/Footer/index.tsx
@@ -1,6 +1,3 @@
-import { use } from 'react'
-import { RenderBuilderContent } from '@/components/builder'
-import { fetchFooter } from '@/helper-functions/builder-fetch'
 import MarginSection from '@/components/_sections/MarginSection'
 import TextContent from '@/components/_styled/Text'
 import {
@@ -9,9 +6,13 @@ import {
   COLORS,
   FONT_STYLE
 } from '@/definitions/enums'
+import { FooterI } from '@/definitions/interfaces/_navigation'
+import SocialLinksSection from '@/components/_sections/SocialLinksSection'
 
-const Footer: React.FC = () => {
-  const content = use(fetchFooter())
+const Footer: React.FC<FooterI> = ({
+  siteDescription,
+  socialLinks
+}) => {
   return (
     <footer className='bg-gray max-h-fit'>
       <MarginSection>
@@ -23,7 +24,29 @@ const Footer: React.FC = () => {
             color={COLORS.WHITE}
             fontStyle={FONT_STYLE.SEMI_BOLD}
           />
-          <RenderBuilderContent content={content} model='footer' />
+          <div className='mt-5'>
+            <TextContent
+              text={siteDescription}
+              fontFamily={FONT_FAMILY.SANS}
+              fontSize={FONT_SIZE.XXL}
+              color={COLORS.WHITE}
+              fontStyle={FONT_STYLE.MEDIUM}
+            />
+          </div>
+          {(socialLinks && socialLinks.length > 0) &&
+            <div className='mt-5'>
+              <SocialLinksSection socialLinks={socialLinks} />
+            </div>
+          }
+          <div className='mt-5'>
+            <TextContent
+              text={`Created © ${new Date().getFullYear()}`}
+              fontFamily={FONT_FAMILY.SANS}
+              fontSize={FONT_SIZE.MD}
+              color={COLORS.WHITE}
+              fontStyle={FONT_STYLE.NORMAL}
+            />
+          </div>
         </section>
       </MarginSection>
     </footer>
