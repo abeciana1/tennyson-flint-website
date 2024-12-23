7ddda779d3321684b35ab4256a5aaf4b0aff4935
abeciana1 <alex.beciana@gmail.com>
2024-12-23 17:26:51 -0600

    Added rendering and props

--- components/_sections/BlogSection/index.tsx
+++ components/_sections/BlogSection/index.tsx
@@ -1,7 +1,31 @@
+import { BlogSectionI } from '@/definitions/interfaces/_sections'
+import { Heading1 } from '@/components/_styled/headings'
+import TextContent from '@/components/_styled/Text'
+import {
+  FONT_STYLE,
+  FONT_FAMILY
+} from '@/definitions/enums'
+import MarginSection from '@/components/_sections/MarginSection'
 
-const BlogSection = () => {
+const BlogSection: React.FC<BlogSectionI> = ({
+  preheading,
+  headline
+}) => {
   return (
-    <></>
+    <section>
+      <MarginSection>
+        <Heading1
+          text={preheading}
+          fontSize='2xl'
+        />
+        <TextContent
+          text={headline}
+          dataTestId='headline'
+          fontStyle={FONT_STYLE.MEDIUM}
+          fontFamily={FONT_FAMILY.CINZEL}
+        />
+      </MarginSection>
+    </section>
   )
 }
 
