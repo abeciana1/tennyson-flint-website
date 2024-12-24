9f2a188e1300c4465513f5c4df530bbe6a84b66b
abeciana1 <alex.beciana@gmail.com>
2024-12-24 17:39:34 -0600

    Added link to hyperlink site title to homepage

--- components/_navigation/NavBar/index.tsx
+++ components/_navigation/NavBar/index.tsx
@@ -3,14 +3,16 @@ import { Heading1 } from '@/components/_styled/headings'
 import NavLink from '@/components/_navigation/NavLink'
 import MobileMenu from '@/components/_navigation/MobileMenu'
 import { fetchNavLinks } from '@/helper-functions/builder-fetch'
-
+import Link from 'next/link'
 
 const NavBar = () => {
   const navLinks = use(fetchNavLinks())
   return (
     <header>
       <nav className='flex flex-row px-5 sm:px-10 py-3 items-center justify-between z-50 bg-white'>
-        <Heading1 text='Tennyson Flint' fontSize='2xl' />
+        <Link href='/' title='Link to homepage'>
+          <Heading1 text='Tennyson Flint' fontSize='2xl' />
+        </Link>
         <ul className='hidden md:flex flex-row gap-6'>
           {navLinks && navLinks?.map((link) => {
             return (
