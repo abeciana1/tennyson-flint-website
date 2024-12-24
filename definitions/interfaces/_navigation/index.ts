74071f89ad244425d2c2edb0660bc678ec2dcf46
abeciana1 <alex.beciana@gmail.com>
2024-12-24 09:18:28 -0600

    Removed unused import and deleted unused interface

--- definitions/interfaces/_navigation/index.ts
+++ definitions/interfaces/_navigation/index.ts
@@ -1,11 +1,4 @@
-import { SocialLinkI } from '@/definitions/interfaces/_styled/Links'
-
 export interface NavLinkI {
   label: string;
   href: string;
-}
-
-export interface FooterI {
-  siteDescription: string;
-  socialLinks: SocialLinkI[];
 }
