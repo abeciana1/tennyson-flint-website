273cddd0c39eac8c043e6ef71320f04586b3d61e
abeciana1 <alex.beciana@gmail.com>
2024-12-24 09:16:14 -0600

    Changed margin to padding

--- components/_sections/MarginSection/index.tsx
+++ components/_sections/MarginSection/index.tsx
@@ -4,7 +4,7 @@ const MarginSection: React.FC<MarginSectionI> = ({
   children
 }) => {
   return (
-    <section className='px-5 sm:px-10 my-4 md:my-8 lg:my-16 relative flex-grow'>
+    <section className='px-5 sm:px-10 py-4 md:py-8 lg:py-16 relative flex-grow'>
       {children}
     </section>
   )
