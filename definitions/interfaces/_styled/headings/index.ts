d9fb84215d3ed5667f9c4f0dfb8e7b0c5c9b0779
abeciana1 <alex.beciana@gmail.com>
2024-12-24 17:55:37 -0600

    Added fontFamily to prop

--- definitions/interfaces/_styled/headings/index.ts
+++ definitions/interfaces/_styled/headings/index.ts
@@ -1,8 +1,9 @@
-import { COLORS, FONT_STYLE } from '@/definitions/enums'
+import { COLORS, FONT_STYLE, FONT_FAMILY } from '@/definitions/enums'
 
 export interface HeadingI {
   text: string;
   fontSize?: string;
   color?: COLORS;
   fontStyle?: FONT_STYLE;
+  fontFamily?: FONT_FAMILY;
 }
