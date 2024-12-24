187f49d5fe2518fd1753434eb75f40b862d7e7fe
abeciana1 <alex.beciana@gmail.com>
2024-12-24 11:41:47 -0600

    Added content rendering for top of blog template

--- components/_blog/BlogTemplate/index.tsx
+++ components/_blog/BlogTemplate/index.tsx
@@ -1,7 +1,66 @@
+import { BlogTemplateI } from '@/definitions/interfaces/_blog'
+import { Heading1 } from '@/components/_styled/headings'
+import MarginSection from '@/components/_sections/MarginSection'
+import TextContent from '@/components/_styled/Text'
+import {
+  FONT_SIZE,
+  FONT_STYLE
+} from '@/definitions/enums'
+import ImageComp from '@/components/_styled/ImageComp'
 
-const BlogTemplate: React.FC = () => {
+const BlogTemplate: React.FC<BlogTemplateI> = ({
+  title,
+  excerpt,
+  image,
+  publishedDate
+}) => {
+  const {
+    src,
+    alt,
+    width,
+    height,
+    rounded
+  } = image
   return (
-    <></>
+    <MarginSection>
+      <MarginSection>
+        <Heading1
+          text={title}
+        />
+        {excerpt &&
+          <TextContent
+            dataTestId='blogExcerpt'
+            text={excerpt}
+            fontSize={FONT_SIZE.XXL}
+            fontStyle={FONT_STYLE.ITALIC}
+          />
+        }
+        <section>
+          <div
+            className='flex justify-between'
+          >
+            <TextContent
+              text='By Tennyson Flint'
+              fontSize={FONT_SIZE.XXL}
+              fontStyle={FONT_STYLE.MEDIUM}
+            />
+            <TextContent
+              dataTestId='publishedDate'
+              text={publishedDate}
+              fontSize={FONT_SIZE.XXL}
+              fontStyle={FONT_STYLE.MEDIUM}
+            />
+          </div>
+          <ImageComp
+            src={src}
+            alt={alt}
+            width={width}
+            height={height}
+            rounded={rounded}
+          />
+        </section>
+      </MarginSection>
+    </MarginSection>
   )
 }
 
