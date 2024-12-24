36b12a8313f5c650103f6eabefd2b698baf7e566
abeciana1 <alex.beciana@gmail.com>
2024-12-24 11:01:35 -0600

    Created initial tests for BlogTemplate component

---
+++ components/_blog/BlogTemplate/__test__/index.test.tsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import { ROUNDED } from '@/definitions/enums'
+import { format } from 'date-fns'
+import BlogTemplate from '@/components/_blog/BlogTemplate'
+
+describe('BlogTemplate', () => {
+  beforeEach(() => {
+    render(
+      <BlogTemplate
+        title="Blog Template Title"
+        excerpt="Blog Template Excerpt"
+        image={{
+          src: '/test.jpg',
+          alt: 'Test Image',
+          width: 100,
+          height: 100,
+          rounded: ROUNDED.MD
+        }}
+        publishedDate={format(new Date(), "MM/dd/yyyy")}
+      />
+    )
+  })
+  test('should render a title', () => {
+    const title = screen.getByRole('heading', { level: 1 });
+    expect(title.textContent).toBe('Blog Template Title');
+    expect(title).toBeInTheDocument();
+  })
+  test('should render an excerpt if present', () => {
+    const excerpt = screen.getByTestId('blogExcerpt')
+    expect(excerpt.textContent).toBe('Blog Template Excerpt');
+    expect(excerpt).toBeInTheDocument();
+  })
+  test('should render an image', () => {
+    const img = screen.getByRole('img')
+    expect(img).toHaveAttribute('src', '/test.jpg')
+    expect(img).toHaveAttribute('alt', 'Test Image')
+    expect(img).toBeInTheDocument()
+  })
+  test('should render a published date', () => {
+    const publishedDate = screen.getByTestId('publishedDate')
+    expect(publishedDate).toBeInTheDocument();
+  })
+})
