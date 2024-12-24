1ce1efba4423d7a26a0ad8427247ab0e049857f7
abeciana1 <alex.beciana@gmail.com>
2024-12-24 16:36:59 -0600

    Added rendering of content

--- components/_blog/BlogImage/index.tsx
+++ components/_blog/BlogImage/index.tsx
@@ -1,7 +1,24 @@
+import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
+import { ROUNDED } from '@/definitions/enums'
+import ImageComp from '@/components/_styled/ImageComp'
 
-const BlogImage: React.FC = () => {
+const BlogImage: React.FC<ImageI> = ({
+  src,
+  alt,
+  width,
+  height,
+  rounded = ROUNDED.MD
+}) => {
   return (
-    <></>
+    <section className='px-5 sm:px-20'>
+      <ImageComp
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        rounded={rounded}
+      />
+    </section>
   )
 }
 
