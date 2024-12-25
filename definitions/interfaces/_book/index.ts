950390b3c3074ae91096cc13fec8788c0ce79593
abeciana1 <alex.beciana@gmail.com>
2024-12-25 13:13:38 -0600

    Added content prop to interface

--- definitions/interfaces/_book/index.ts
+++ definitions/interfaces/_book/index.ts
@@ -1,7 +1,9 @@
 import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
+import { BuilderContent } from '@builder.io/sdk';
 
 export interface BookDetailTemplateI {
   preheading: string;
   bookTitle: string;
   bookCover: ImageI;
+  content: BuilderContent;
 }
