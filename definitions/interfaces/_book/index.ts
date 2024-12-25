95f29a95b5c9b0abaa0b3f2213199187b08d5db8
abeciana1 <alex.beciana@gmail.com>
2024-12-25 17:44:15 -0600

    Removed unused import and prop

--- definitions/interfaces/_book/index.ts
+++ definitions/interfaces/_book/index.ts
@@ -1,9 +1,7 @@
 import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
-import { BuilderContent } from '@builder.io/sdk';
 
 export interface BookDetailTemplateI {
   preheading: string;
   bookTitle: string;
   bookCover: ImageI;
-  content: BuilderContent;
 }
