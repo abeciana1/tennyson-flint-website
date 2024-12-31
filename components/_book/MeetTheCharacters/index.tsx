769bd6783afab8c5f6565464734a13c48b1d75cb
abeciana1 <alex.beciana@gmail.com>
2024-12-31 16:45:15 -0700

    Created MeetTheCharacters component

---
+++ components/_book/MeetTheCharacters/index.tsx
@@ -0,0 +1,14 @@
+import MarginSection from '@/components/_sections/MarginSection'
+import { Heading2 } from '@/components/_styled/headings'
+
+const MeetTheCharacters = () => {
+  return (
+    <MarginSection>
+      <Heading2
+        text="Meet the Characters"
+      />
+    </MarginSection>
+  )
+}
+
+export default MeetTheCharacters
