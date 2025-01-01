27b387da94a34404bbb05dc319e6f4bc2349d595
abeciana1 <alex.beciana@gmail.com>
2024-12-31 20:54:57 -0700

    Imported CharacterI interface && Added conditional rendering and mapping of character data

--- components/_book/MeetTheCharacters/index.tsx
+++ components/_book/MeetTheCharacters/index.tsx
@@ -1,16 +1,24 @@
 import MarginSection from '@/components/_sections/MarginSection'
 import { Heading2 } from '@/components/_styled/headings'
-import { MeetTheCharactersI } from '@/definitions/interfaces/_book'
+import { MeetTheCharactersI, CharacterI } from '@/definitions/interfaces/_book'
 
 const MeetTheCharacters: React.FC<MeetTheCharactersI> = ({
   headingText,
-  bookSlug
+  builderState
 }) => {
+  console.log('builderState', builderState)
   return (
     <MarginSection>
       <Heading2
         text={headingText}
       />
+      {(builderState?.content?.data?.state?.character?.results) &&
+        <div className='flex flex-wrap'>
+          {builderState?.content?.data?.state?.character?.results?.map((char): CharacterI => {
+            return (<></>)
+          })}
+        </div>
+      }
     </MarginSection>
   )
 }
