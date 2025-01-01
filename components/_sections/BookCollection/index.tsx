50a4c316d91bfe64e6969a878decd113d42e7b2f
abeciana1 <alex.beciana@gmail.com>
2025-01-01 15:49:36 -0700

    Added rendering of book section and data

--- components/_sections/BookCollection/index.tsx
+++ components/_sections/BookCollection/index.tsx
@@ -1,15 +1,35 @@
 import { BookCollectionI, BookI } from '@/definitions/interfaces/_book'
 import MarginSection from '@/components/_sections/MarginSection'
-import { ROUNDED } from '@/definitions/enums'
+import {
+  ROUNDED,
+  FONT_FAMILY,
+  FONT_SIZE
+} from '@/definitions/enums'
 import BookCard from '@/components/_book/BookCard'
+import TextContent from '@/components/_styled/Text'
+
+const MoreBooksComingSoon = () => {
+  return (
+    <div className='relative flex flex-col items-center bg-gray text-center lg:max-h-56 max-w-3xl p-10 rounded-lg'>
+      <TextContent
+        text='The start of my book collection'
+        fontFamily={FONT_FAMILY.CINZEL_DEC}
+        fontSize={FONT_SIZE.XXXL}
+      />
+      <TextContent
+        text='More work coming soon, stay tuned!'
+        fontSize={FONT_SIZE.XXXL}
+      />
+    </div>
+  )
+}
 
 const BookCollection: React.FC<BookCollectionI> = ({ builderState }) => {
-  console.log('builderState', builderState)
   const books = builderState?.content?.data?.state?.bookPage?.results
   return (
     <MarginSection>
       {(books) &&
-        <section className='flex'>
+        <section className='flex flex-col md:flex-row gap-12 md:gap-36 items-center'>
           {(books) &&
             books.map(({data}: BookI, index: number) => {
               return (
@@ -20,13 +40,16 @@ const BookCollection: React.FC<BookCollectionI> = ({ builderState }) => {
                   bookCover={{
                     src: data?.bookImage,
                     alt: data?.bookTitle,
-                    width: data?.bookImageWidth,
-                    height: data?.bookImageHeight,
+                    width: 237,
+                    height: 385,
                     rounded: ROUNDED.MD,
                   }}
                 />
               )
             })
+          }
+          {books && (books?.length < 3) &&
+            <MoreBooksComingSoon />
           }
         </section>
       }
