92fe63526135263c42984d66265fa5b224a0b044
abeciana1 <alex.beciana@gmail.com>
2024-12-31 17:36:19 -0700

    Removed unused import

--- components/_book/MeetTheCharacters/index.tsx
+++ components/_book/MeetTheCharacters/index.tsx
@@ -1,7 +1,6 @@
 import MarginSection from '@/components/_sections/MarginSection'
 import { Heading2 } from '@/components/_styled/headings'
 import { MeetTheCharactersI } from '@/definitions/interfaces/_book'
-import { BuilderBlocks } from '@builder.io/react'
 
 const MeetTheCharacters: React.FC<MeetTheCharactersI> = ({
   headingText,
