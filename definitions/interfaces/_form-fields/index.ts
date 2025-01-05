746f3f9ba2676b67bba49fb9e6f70fadb5c8cf3a
abeciana1 <alex.beciana@gmail.com>
2025-01-04 20:57:00 -0700

    Added name and register props

--- definitions/interfaces/_form-fields/index.ts
+++ definitions/interfaces/_form-fields/index.ts
@@ -1,8 +1,11 @@
 import { FIELD_TYPE } from "@/definitions/enums";
+import { UseFormRegister, FieldValues } from 'react-hook-form'
 
 export interface ShortTextFieldI {
   type: FIELD_TYPE;
   label: string;
   required?: boolean;
   helperText?: string;
+  register: UseFormRegister<FieldValues>;
+  name: string;
 }
