18f106841145291a5002b3923b7f83d36a1eff47
abeciana1 <alex.beciana@gmail.com>
2025-01-05 00:06:51 -0700

    Created test file for TextArea component

---
+++ components/_form-fields/TextArea/__test__/index.test.tsx
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import TextArea from '@/components/_form-fields/TextArea'
+import { useForm, FormProvider } from 'react-hook-form';
+
+const Wrapper = ({ children }: {children: React.ReactNode[] | React.ReactNode}) => {
+  const methods = useForm();
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
+
+describe('TextArea', () => {
+  const mockRegister = jest.fn();
+  beforeEach(() => {
+    render(
+      <Wrapper>
+        <TextArea
+          label='Text Area'
+          name='textArea'
+          required
+          helperText='helper text'
+          register={mockRegister}
+        />
+      </Wrapper>
+    )
+  })
+  test('should render textbox role', () => {
+    const field = screen.getByRole('textbox')
+    expect(field).toHaveAttribute('id', 'textArea')
+    expect(field).toBeInTheDocument()
+  })
+})
