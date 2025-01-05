ed07be5eb78ff850e5d8ae917767b189af204da1
abeciana1 <alex.beciana@gmail.com>
2025-01-04 21:02:51 -0700

    Fixed failing tests

--- components/_form-fields/ShortTextField/__test__/index.test.tsx
+++ components/_form-fields/ShortTextField/__test__/index.test.tsx
@@ -2,26 +2,36 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import ShortTextField from '@/components/_form-fields/ShortTextField'
 import { FIELD_TYPE } from '@/definitions/enums'
+import { useForm, FormProvider } from 'react-hook-form';
 import { slugify } from '@/helper-functions/general'
+import { ReactNode } from 'react';
+
+const Wrapper = ({ children }: {children: ReactNode[] | ReactNode}) => {
+  const methods = useForm();
+  return <FormProvider {...methods}>{children}</FormProvider>;
+};
 
 describe('ShortTextField', () => {
+  const mockRegister = jest.fn();
   beforeEach(() => {
     render(
-      <ShortTextField
-        type={FIELD_TYPE.TEXT}
-        label='Test label'
-        required={true}
-        helperText='Enter text here'
-      />
+      <Wrapper>
+        <ShortTextField
+          name={slugify('Test label')}
+          type={FIELD_TYPE.TEXT}
+          label='Test label'
+          required
+          helperText='Enter text here'
+          register={mockRegister}
+        />
+      </Wrapper>
     )
   })
   test('should render textbox role', () => {
-    const field = screen.getByRole('textbox', {
-      name: 'test-label'
-    })
-    expect(field).toHaveAttribute('placeholder', 'Test label')
-    expect(field).toHaveAttribute('required', 'true')
-    expect(field).toHaveAttribute('name', 'test-label')
+    const field = screen.getByRole('textbox')
+    expect(field).toHaveAttribute('placeholder', 'Test label*')
+    expect(field).toHaveAttribute('id', 'test-label')
+    expect(field).toHaveAttribute('type', 'text')
     expect(field).toBeInTheDocument()
   })
 })
