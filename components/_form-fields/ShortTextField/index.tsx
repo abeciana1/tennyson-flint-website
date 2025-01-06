015c55e95696c98308cf9d21d25531cff945bb7a
abeciana1 <alex.beciana@gmail.com>
2025-01-05 17:51:55 -0700

    Adjusted styling for validation and error messaging

--- components/_form-fields/ShortTextField/index.tsx
+++ components/_form-fields/ShortTextField/index.tsx
@@ -1,4 +1,7 @@
 import { ShortTextFieldI } from '@/definitions/interfaces/_form-fields'
+import { COLORS } from '@/definitions/enums'
+import cx from 'classnames'
+import { validateEmail } from '@/helper-functions/general'
 
 const ShortTextField: React.FC<ShortTextFieldI> = ({
   name,
@@ -6,28 +9,54 @@ const ShortTextField: React.FC<ShortTextFieldI> = ({
   label,
   required = false,
   helperText = '',
-  register
+  register,
+  textColor,
+  errors
 }) => {
   return (
-    <div className='flex flex-col grow'>
+    <div className='flex flex-col grow relative'>
       <label
         htmlFor={name}
         className='sr-only'
       >
         {label + (required && '*')}
       </label>
+      {errors[name] &&
+          <div
+            role='alert'
+            className='text-red font-sans text-sm absolute -top-6'
+          >
+            {errors[name]?.message}
+          </div>
+      }
       <input
         id={name}
         type={type}
         placeholder={label + (required && '*')}
         {...register(name, {
           required: required && 'This field is required',
+          validate: value => {
+            if (type === 'email') {
+              const isValid = validateEmail(value);
+              return isValid || 'The email address is invalid.';
+            }
+            return true;
+          }
         })}
         className='max-h-12 py-3 px-3 rounded-lg font-sans w-full'
+        aria-invalid={errors[name] ? 'true' : 'false'}
       />
       {helperText &&
         <small
-          className=''
+          className={cx('text-sm font-sans font-medium', {
+            ['text-navy']: textColor === COLORS.NAVY,
+            ['text-white']: textColor === COLORS.WHITE,
+            ['text-black']: textColor === COLORS.BLACK,
+            ['text-gold']: textColor === COLORS.GOLD,
+            ['text-violet']: textColor === COLORS.VIOLET,
+            ['text-red']: textColor === COLORS.RED,
+            ['text-pink']: textColor === COLORS.PINK,
+          })}
         >
           {helperText}
         </small>
--- components/_form-fields/TextArea/index.tsx
+++ components/_form-fields/TextArea/index.tsx
@@ -1,20 +1,32 @@
 import { TextAreaI } from '@/definitions/interfaces/_form-fields'
+import { COLORS } from '@/definitions/enums'
+import cx from 'classnames'
 
 const TextArea:React.FC<TextAreaI> = ({
   label,
   required,
   helperText,
   register,
-  name
+  name,
+  textColor,
+  errors
 }) => {
   return (
-    <div className='flex flex-col grow'>
+    <div className='flex flex-col grow relative'>
       <label
         htmlFor={name}
         className='sr-only'
       >
         {label + (required && '*')}
       </label>
+      {errors[name] &&
+          <div
+            role='alert'
+            className='text-red font-sans text-sm absolute -top-6'
+          >
+            {errors[name]?.message}
+          </div>
+      }
       <textarea
         id={name}
         {...register(name, {
@@ -22,10 +34,19 @@ const TextArea:React.FC<TextAreaI> = ({
         })}
         className='py-3 px-3 rounded-lg h-40 font-sans'
         placeholder={label + (required && '*')}
+        aria-invalid={errors[name] ? 'true' : 'false'}
       />
       {helperText &&
         <small
-          className=''
+          className={cx('text-sm font-sans font-medium', {
+            ['text-navy']: textColor === COLORS.NAVY,
+            ['text-white']: textColor === COLORS.WHITE,
+            ['text-black']: textColor === COLORS.BLACK,
+            ['text-gold']: textColor === COLORS.GOLD,
+            ['text-violet']: textColor === COLORS.VIOLET,
+            ['text-red']: textColor === COLORS.RED,
+            ['text-pink']: textColor === COLORS.PINK,
+          })}
         >
           {helperText}
         </small>
