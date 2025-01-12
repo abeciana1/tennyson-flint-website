9608d60eb5a67fae4edd0a66e05d8e3422036919
abeciana1 <alex.beciana@gmail.com>
2025-01-11 23:05:44 -0700

    Added renderings for json ld

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -23,30 +23,43 @@ export const generateMetadata = async (props: PageProps): Promise<Metadata> => {
   const content = await pageContentDataFetch(props, modelName)
 
   if (modelName === 'blog-article') {
+    console.log('content: ', content)
     return {
-      title: content?.data?.blogPostTitle + '!!!!!!',
+      title: content?.data?.blogPostTitle,
       description: content?.data?.seoDescription,
       alternates: {
         canonical: `/blog/${urlStructure?.page[1]}`
+      },
+      openGraph: {
+        title: `${content?.data?.blogPostTitle} | Tennyson Flint`,
+        description: content?.data?.seoDescription,
+        images: [content?.data?.blogImage]
       }
     }
-    // add json-ld for books
   } else if (modelName === 'book-page') {
-    console.log('content: ', content)
     return {
-      title: content?.name + '!!!!',
-      description: content?.data?.description,
+      title: content?.data?.bookTitle,
+      description: content?.data?.seoDescription,
       alternates: {
         canonical: `/book/${urlStructure?.page[1]}`
+      },
+      openGraph: {
+        title: `${content?.data?.bookTitle} | Tennyson Flint`,
+        description: content?.data?.seoDescription,
+        images: [content?.data?.bookImage]
       }
     }
-    // add json-ld for books
   } else {
     return {
       title: content?.data?.title,
       description: content?.data?.description,
       alternates: {
         canonical: `/${urlStructure?.page[1]}`
+      },
+      openGraph: {
+        title: content?.data?.title,
+        description: content?.data?.description,
+        images: [content?.data?.openGraphImage]
       }
     }
   }
@@ -55,8 +68,55 @@ export const generateMetadata = async (props: PageProps): Promise<Metadata> => {
 export default async function Page(props: PageProps) {
   const { modelName } = await modelAndContentFetch(props)
   const content = await pageContentDataFetch(props, modelName)
+
+  let jsonLD;
+
+  if (modelName === 'book-page') {
+    jsonLD = {
+      '@context': 'https://schema.org',
+      "@type": "WebPage",
+      "mainEntity": {
+        "@type": "Book",
+        "author": "https://tennysonflint.com/about",
+        "image": content?.data?.bookImage,
+        "inLanguage": "English",
+        "name": content?.data?.bookTitle,
+        "numberOfPages": content?.data?.numberOfPages,
+        "datePublished": format(new Date(content?.data?.bookPublishedDate), 'yyyy-MM-dd')
+      }
+    }
+  } else if (modelName === 'blog-article') {
+    jsonLD = {
+      "@context":"http://schema.org",
+      "@type": "BlogPosting",
+      "image": content?.data?.blogImage,
+      "url": `https://tennysonflint.com/blog/${content?.data?.slug}`,
+      "headline": content?.data?.bookTitle,
+      "dateCreated": format(new Date(content?.createdDate), 'yyyy-MM-dd'),
+      "dataPublished": format(new Date(content?.firstPublished), 'yyyy-MM-dd'),
+      "copyrightYear": format(new Date(content?.firstPublished), 'yyyy'),
+      "inLanguage": "en-US",
+      "author": {
+        "@type": "Person",
+        "name": "Tennyson Flint",
+        "url": "https://tennysonflint.com"
+      }
+    }
+  } else {
+    jsonLD = {
+      "@context":"http://schema.org",
+      "@type": "WebPage",
+      "name": `${content?.data?.bookTitle} | Tennyson Flint`,
+      "description": content?.data?.description,
+    }
+  }
+
+  const stringifiedLD = JSON.stringify(jsonLD)
   return (
     <>
+      {jsonLD &&
+        <script type="application/ld+json">{ stringifiedLD }</script>
+      }
       <div className='relative'>
         <main className={cx('min-h-screen relative', {
           ['pb-20']: modelName === 'blog-article'
