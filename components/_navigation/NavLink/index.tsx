d1082fc3c16a103bdd3bffed3a93f03333db3809
abeciana1 <alex.beciana@gmail.com>
2025-01-12 21:00:20 -0700

    Removed commented out code

--- components/_navigation/NavLink/index.tsx
+++ components/_navigation/NavLink/index.tsx
@@ -1,22 +1,15 @@
-// 'use client'
 import { NavLinkI } from '@/definitions/interfaces/_navigation'
 import Link from 'next/link'
-// import { usePathname } from 'next/navigation'
-import cx from 'classnames'
 
 const NavLink: React.FC<NavLinkI> = ({
   label,
   href
 }) => {
-  // const pathname = usePathname()
-  // const isActive = pathname === href
   return (
     <li>
       <Link
         href={href}
-        className={cx('text-xl font-medium font-sans', {
-          // ['text-navy underline decoration-navy underline-offset-2']: isActive
-        })}
+        className='text-xl font-medium font-sans'
       >
         {label}
       </Link>
