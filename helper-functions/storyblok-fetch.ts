0a6438a3f238abfe3a7585a1300acfaccd2ef77b
abeciana1 <alex.beciana@gmail.com>
2025-01-12 21:24:42 -0700

    Removed extra and redundant code

--- helper-functions/storyblok-fetch.ts
+++ helper-functions/storyblok-fetch.ts
@@ -1,24 +1,13 @@
-import { ISbResponse, getStoryblokApi, ISbStoriesParams, StoryblokClient } from "@storyblok/react/rsc";
-
-export async function fetchData() {
-  let sbParams: ISbStoriesParams = { version: 'draft' };
-
-  const storyblokApi: StoryblokClient = getStoryblokApi();
-  return storyblokApi.get(`cdn/stories/home`, sbParams);
-}
+import {
+  getStoryblokApi,
+  StoryblokClient
+} from "@storyblok/react/rsc";
 
 export const fetchStory = async (
   version: "draft" | "published",
   slug?: string[]
 ) => {
-  getStoryblokApi();
   const correctSlug = `/${slug ? slug.join("/") : "home"}`;
-  return fetch(
-    `
-    https://api.storyblok.com/v2/cdn/stories${correctSlug}?version=${version}&token=${process.env.NEXT_PUBLIC_STORYBLOK_TOKEN}`,
-    {
-      next: { tags: ["cms"] },
-      cache: version === "published" ? "default" : "no-store",
-    }
-  ).then((res) => res.json()) as Promise<{ story: ISbResponse }>;
+  const storyblokApi: StoryblokClient = getStoryblokApi();
+  return storyblokApi.get(`cdn/stories/${correctSlug}`, { version: version});
 };
