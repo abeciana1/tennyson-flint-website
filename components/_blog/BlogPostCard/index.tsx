89f0b4e4b4350bf0bc4972e30677fcae49142b41
abeciana1 <alex.beciana@gmail.com>
2025-01-13 21:38:22 -0700

    Adjusted props and data mapping && Removed unneeded Link component

--- components/_blog/BlogPostCard/index.tsx
+++ components/_blog/BlogPostCard/index.tsx
@@ -10,7 +10,6 @@ import {
 import { ButtonLink } from '@/components/_styled/links'
 import BlogDate from '@/components/_blog/BlogDate'
 import BlogCategory from '@/components/_blog/BlogCategory'
-import Link from 'next/link'
 
 const BlogPostCard: React.FC<BlogPostCardI> = ({
   title,
@@ -21,64 +20,62 @@ const BlogPostCard: React.FC<BlogPostCardI> = ({
   category
 }) => {
   const {
-    src,
-    alt,
+    file,
+    alt_text,
     width,
     height,
-    rounded
+    rounded_edges
   } = image
   return (
-    <Link href={href} title={title}>
-      <div className='p-6 max-w-[300px]'>
-        <div className='relative max-w-60 max-h-40'>
-          <ImageComp
-            src={src}
-            alt={alt}
-            width={width}
-            height={height}
-            rounded={rounded}
+    <div className='p-6 max-w-[300px]'>
+      <div className='relative max-w-60 max-h-40'>
+        <ImageComp
+          file={file}
+          alt_text={alt_text}
+          width={width}
+          height={height}
+          rounded_edges={rounded_edges}
+        />
+        <div className='absolute -bottom-8 right-4 text-center'>
+          <BlogDate
+            month={publishedDate.month}
+            day={publishedDate.day}
           />
-          <div className='absolute -bottom-8 right-4 text-center'>
-            <BlogDate
-              month={publishedDate.month}
-              day={publishedDate.day}
-            />
-          </div>
         </div>
-        <div className='mt-6'>
-          <div className='mt-4'>
-            <BlogCategory
-              id={category}
-            />
-          </div>
-          <div className='mt-2'>
-            <TextContent
-              dataTestId='title'
-              text={title}
-              fontSize={FONT_SIZE.XL}
-              fontStyle={FONT_STYLE.SEMI_BOLD}
-            />
-          </div>
-        </div>
+      </div>
+      <div className='mt-6'>
         <div className='mt-4'>
-          <TextContent
-            dataTestId='excerpt'
-            text={excerpt?.substring(0, 46) + '...'}
-            fontSize={FONT_SIZE.XL}
+          <BlogCategory
+            name={category}
           />
         </div>
-        <div className='mt-5'>
-          <ButtonLink
-            linkText='Read more'
-            title='Read more about this article'
-            href={href}
-            bgColor={COLORS.PINK}
-            rounded={ROUNDED.MD}
+        <div className='mt-2'>
+          <TextContent
+            dataTestId='title'
+            text={title}
+            fontSize={FONT_SIZE.XL}
             fontStyle={FONT_STYLE.SEMI_BOLD}
           />
         </div>
       </div>
-    </Link>
+      <div className='mt-4'>
+        <TextContent
+          dataTestId='excerpt'
+          text={excerpt?.substring(0, 46) + '...'}
+          fontSize={FONT_SIZE.XL}
+        />
+      </div>
+      <div className='mt-5'>
+        <ButtonLink
+          linkText='Read more'
+          title='Read more about this article'
+          href={href}
+          bgColor={COLORS.PINK}
+          rounded={ROUNDED.MD}
+          fontStyle={FONT_STYLE.SEMI_BOLD}
+        />
+      </div>
+    </div>
   )
 }
 
