38b26381b45617895d9b3ccf9b755f43ebdc0edf
abeciana1 <alex.beciana@gmail.com>
2025-01-13 21:37:45 -0700

    Created fetchContentStories function to retrieve collections

--- helper-functions/storyblok-fetch.ts
+++ helper-functions/storyblok-fetch.ts
@@ -11,3 +11,15 @@ export const fetchStory = async (
   const storyblokApi: StoryblokClient = getStoryblokApi();
   return storyblokApi.get(`cdn/stories/${correctSlug}`, { version: version});
 };
+
+export const fetchContentStories = async (
+  version: "draft" | "published",
+  dirName: string
+) => {
+  const storyblokApi: StoryblokClient = getStoryblokApi();
+  const content = await storyblokApi.get(`cdn/stories`, {
+    version: version,
+    starts_with: dirName
+  })
+  return content
+}
