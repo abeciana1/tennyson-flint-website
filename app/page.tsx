4a92644329f6df390005f069b1b15bdf6cd20aaa
abeciana1 <alex.beciana@gmail.com>
2025-01-15 19:49:40 -0700

    Adjusted story fetch

--- app/page.tsx
+++ app/page.tsx
@@ -6,14 +6,15 @@ import {
 } from "@storyblok/react/rsc";
 
 export default async function Page(props: PageProps) {
-  const content = await fetchStory('published', ['home'])
+  const slug = (await props?.params).page;
+  const content = await fetchStory('published', slug)
   return (
     <>
       <div className='relative'>
         <main className='min-h-screen relative'
           {...storyblokEditable(props.blok)}
         >
-          <StoryblokStory story={content?.story} />
+          <StoryblokStory story={content?.data?.story} />
         </main>
       </div>
     </>
