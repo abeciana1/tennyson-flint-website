960a1cd8429d862a395636cdf662f5992f93a76b
abeciana1 <alex.beciana@gmail.com>
2025-01-15 19:44:34 -0700

    Replaced sanitizing HTML with Markdown and Added link rendering

--- components/_blog/BlogContent/index.tsx
+++ components/_blog/BlogContent/index.tsx
@@ -1,17 +1,39 @@
 import { BlogContentI } from '@/definitions/interfaces/_blog'
-import DOMPurify from 'isomorphic-dompurify';
 import '@/components/_blog/BlogContent/blog-content.css'
+import Markdown from 'react-markdown'
+import Link from 'next/link'
 
 const BlogContent: React.FC<BlogContentI> = ({
-  textContent
+  blok
 }) => {
-  const sanitizedContent = DOMPurify.sanitize(textContent)
+  const {
+    text
+  } = blok
+  console.log('content text', text)
   return (
     <section
       data-testid='blogContent'
-      className='blog-content px-5 sm:px-20'
-      dangerouslySetInnerHTML={{__html: sanitizedContent}}
-    />
+      className='blog-content px-5 sm:px-20 '
+    >
+      <Markdown
+        components={{
+          a(props) {
+            const { node, children } = props
+            const hrefLink = node?.properties?.href as string
+            const isExternal = hrefLink?.match(/http/i)
+            return (
+              <Link
+                href={node?.properties?.href as string}
+                target={isExternal? '_blank' : '_self'}
+              >
+                {children}{isExternal && <span className='text-base font-bold'>_↗</span>}
+              </Link>
+            )
+            
+          }
+        }}
+      >{text}</Markdown>
+    </section>
   )
 }
 
