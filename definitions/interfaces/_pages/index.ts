72ad0841d43335c98e2128a2fc157be64f7d0cf5
abeciana1 <alex.beciana@gmail.com>
2025-01-26 12:58:24 -0600

    Created BookPageI interface

---
+++ definitions/interfaces/_pages/index.ts
@@ -0,0 +1,16 @@
+import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
+import { TextContentI } from '@/definitions/interfaces/_styled/Text'
+import { TabGroupI } from '@/definitions/interfaces/_styled/TabGroup'
+
+export interface BookPageI {
+  blok: {
+    book_title: string;
+    book_cover: ImageI[];
+    preheading: TextContentI[];
+    seo_title: string;
+    seo_description: string;
+    number_of_pages: string | number;
+    _uid: string;
+    tab_group: TabGroupI[];
+  }
+}
