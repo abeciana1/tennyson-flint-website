0cd9c33adce0438dadceacd3e0063cd255021741
abeciana1 <alex.beciana@gmail.com>
2025-01-26 15:31:02 -0600

    Created ButtonLinkDataI interface

--- definitions/interfaces/_styled/Links/index.ts
+++ definitions/interfaces/_styled/Links/index.ts
@@ -2,6 +2,18 @@ import { COLORS, FONT_STYLE, ROUNDED } from '@/definitions/enums'
 import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
 import { ComponentI } from '@/definitions/interfaces'
 
+export interface ButtonLinkDataI extends ComponentI {
+  title: string;
+  href: string;
+  link_text: string;
+  background_color: COLORS;
+  text_color: COLORS;
+  arrow_icon: boolean;
+  target_blank: boolean;
+  rounded_edges: ROUNDED;
+  font_style: FONT_STYLE;
+}
+
 export interface BasicLinkI extends ComponentI {
   title: string;
   href: string;
