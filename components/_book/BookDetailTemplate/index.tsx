ac36478b75242270d604e2e5cbe4d6b2a11cc362
abeciana1 <alex.beciana@gmail.com>
2025-01-26 15:32:13 -0600

    Added rendering for links

--- components/_book/BookDetailTemplate/index.tsx
+++ components/_book/BookDetailTemplate/index.tsx
@@ -1,16 +1,23 @@
 import { Heading1 } from '@/components/_styled/headings'
 import TextContent from '@/components/_styled/Text'
 import ImageComp from '@/components/_styled/ImageComp'
-import { ROUNDED } from '@/definitions/enums'
+import {
+  COLORS,
+  ROUNDED,
+  FONT_STYLE
+} from '@/definitions/enums'
 import MarginSection from '@/components/_sections/MarginSection'
 import { BookDetailTemplateI } from '@/definitions/interfaces/_book'
 import TabGroup from '@/components/_book/TabGroup'
+import { ButtonLinkDataI } from '@/definitions/interfaces/_styled/Links'
+import { ButtonLink } from '@/components/_styled/links'
 
 const BookDetailTemplate: React.FC<BookDetailTemplateI> = ({
   preheading,
   bookTitle,
   bookCover,
-  tabGroup
+  tabGroup,
+  links
 }) => {
   const {
     file,
@@ -52,7 +59,28 @@ const BookDetailTemplate: React.FC<BookDetailTemplateI> = ({
             />
           </div>
           {tabGroup &&
-            <TabGroup tabs={tabGroup?.tabs} />
+            <TabGroup tabs={tabGroup[0]?.tabs} />
+          }
+          {(links && links?.length > 0) &&
+            <div className='mt-6'>
+              {links?.map((link: ButtonLinkDataI) => {
+                console.log({ link })
+                return (
+                  <ButtonLink
+                    key={link._uid}
+                    linkText={link?.link_text}
+                    title={link?.title}
+                    href={link?.href}
+                    targetBlank={link?.target_blank}
+                    textColor={COLORS[link?.text_color]}
+                    arrow={link?.arrow_icon}
+                    bgColor={COLORS[link?.background_color]}
+                    rounded={ROUNDED[link?.rounded_edges]}
+                    fontStyle={FONT_STYLE[link?.font_style]}
+                  />
+                )
+              })}
+            </div>
           }
         </div>
       </section>
