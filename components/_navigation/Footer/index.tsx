cb749b41709f38ea1b1ddb7c536cc70806c3f47e
abeciana1 <alex.beciana@gmail.com>
2025-02-01 16:58:13 -0700

    Removed builder code

--- components/_navigation/Footer/index.tsx
+++ components/_navigation/Footer/index.tsx
@@ -11,18 +11,11 @@ import { fetchStory } from '@/helper-functions/storyblok-fetch'
 
 const Footer: React.FC = () => {
   const content = use(fetchStory('published', ['global-content', 'main-menu']))
+  console.log('footer content', content)
   return (
     <>
       <footer className='bg-darkGray relative bottom-0 w-full px-5 sm:px-10 pt-2 md:pt-4 lg:pt-8'>
         <section className='mx-auto text-center'>
-          <TextContent
-            text='Tennyson Flint'
-            fontFamily={FONT_FAMILY.CINZEL_DEC}
-            fontSize={FONT_SIZE.XXXL}
-            color={COLORS.WHITE}
-            fontStyle={FONT_STYLE.SEMI_BOLD}
-          />
-          <RenderBuilderContent content={content} model='footer' />
         </section>
       </footer>
     </>
