6f10a87ea2adc1c4ec3e9f12d022333073cd6855
abeciana1 <alex.beciana@gmail.com>
2025-02-02 16:01:14 -0700

    Created initial render of 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+
+const NotFound = () => {
+  return (
+    <main className='grow'>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <Link href="/">Return Home</Link>
+    </main>
+  )
+}
+
+export default NotFound
