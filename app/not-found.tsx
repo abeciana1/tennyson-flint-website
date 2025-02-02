5065e4700079eb2af06a747d839c04d5354f2f82
abeciana1 <alex.beciana@gmail.com>
2025-02-02 16:32:00 -0700

    Adjusted content and styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,42 @@
-import Link from 'next/link'
-
+import TextContent from '@/components/_styled/Text'
+import {
+  FONT_FAMILY,
+  FONT_SIZE,
+  COLORS,
+  ROUNDED,
+  FONT_STYLE
+} from '@/definitions/enums'
+import { Heading1 } from '@/components/_styled/headings'
+import { ButtonLink } from '@/components/_styled/links'
 
 const NotFound = () => {
   return (
-    <main className='grow'>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <main className='grow px-2.5'>
+      <section className='flex flex-col justify-center items-center text-center h-screen max-h-[500px]'>
+        <Heading1
+          text='Page Not Found'
+        />
+        <div className='mb-5'>
+          <TextContent
+            blok={{
+              text: 'The page you are looking for does not exist.',
+              font_size: FONT_SIZE.XXL,
+              color: COLORS.BLACK,
+              font_family: FONT_FAMILY.SANS
+            }}
+          />
+        </div>
+        <ButtonLink
+          linkText='Return to the homepage'
+          href='/'
+          title='Return to the homepage'
+          bgColor={COLORS.NAVY}
+          textColor={COLORS.GOLD}
+          rounded={ROUNDED.MD}
+          fontStyle={FONT_STYLE.MEDIUM}
+          arrow
+        />
+      </section>
     </main>
   )
 }
