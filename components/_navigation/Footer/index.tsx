9cf852d278bc9ecda3aede6c5c5ed583df49cf61
abeciana1 <alex.beciana@gmail.com>
2025-02-02 14:16:33 -0700

    Mapped and fetched data for footer menu

--- components/_navigation/Footer/index.tsx
+++ components/_navigation/Footer/index.tsx
@@ -1,14 +1,16 @@
-// import TextContent from '@/components/_styled/Text'
 import { use } from 'react'
 import { fetchStory } from '@/helper-functions/storyblok-fetch'
+import { storyblokEditable, StoryblokStory } from '@storyblok/react/rsc';
 
 const Footer: React.FC = () => {
-  const content = use(fetchStory('published', ['global-content', 'main-menu']))
-  console.log('footer content', content)
+  const footerContent = use(fetchStory('published', ['global-content', 'footer-menu']))
   return (
     <>
-      <footer className='bg-darkGray relative bottom-0 w-full px-5 sm:px-10 pt-2 md:pt-4 lg:pt-8'>
-        <section className='mx-auto text-center'>
+      <footer
+        className='bg-darkGray relative bottom-0 w-full px-5 sm:px-10 pt-2 md:pt-4 lg:pt-8'
+      >
+        <section className='mx-auto text-center' {...storyblokEditable(footerContent?.data?.story?.content)}>
+          <StoryblokStory story={footerContent?.data?.story} />
         </section>
       </footer>
     </>
