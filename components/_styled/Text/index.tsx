aba4cbc224de056f98575c688371925c288df854
abeciana1 <alex.beciana@gmail.com>
2025-02-02 14:15:39 -0700

    Adjusted properties and changed casings

--- components/_styled/Text/index.tsx
+++ components/_styled/Text/index.tsx
@@ -7,8 +7,9 @@ import {
 } from "@/definitions/enums";
 import cx from 'classnames'
 import '@/components/_blog/BlogContent/blog-content.css'
-import Markdown from 'react-markdown'
 import Link from 'next/link'
+import { storyblokEditable } from '@storyblok/react/rsc';
+import Markdown from 'react-markdown';
 
 export const MarkdownText: React.FC<MarkdownTextI> = ({
   text
@@ -17,7 +18,7 @@ export const MarkdownText: React.FC<MarkdownTextI> = ({
     <div>
       <Markdown
         components={{
-          a(props) {
+          a(props: any) {
             const { node, children } = props
             const hrefLink = node?.properties?.href as string
             const isExternal = hrefLink?.match(/http/i)
@@ -38,32 +39,36 @@ export const MarkdownText: React.FC<MarkdownTextI> = ({
 }
 
 const TextContent: React.FC<TextContentI> = ({
-  text,
-  fontSize = FONT_SIZE.LG,
-  fontStyle = FONT_STYLE.NORMAL,
-  fontFamily = FONT_FAMILY.SANS,
-  color = COLORS.BLACK,
-  dataTestId = ''
+  blok
 }) => {
+  const {
+    text,
+    font_size = FONT_SIZE.LG,
+    font_style = FONT_STYLE.NORMAL,
+    font_family = FONT_FAMILY.SANS,
+    color = COLORS.BLACK,
+    data_test_id = ''
+  } = blok
   return (
     <div
-      data-testid={dataTestId}
+      {...storyblokEditable(blok)}
+      data-testid={data_test_id}
       className={cx('leading-normal', {
-        ['font-bold']: fontStyle === FONT_STYLE.BOLD,
-        ['font-semibold']: fontStyle === FONT_STYLE.SEMI_BOLD,
-        ['font-medium']: fontStyle === FONT_STYLE.MEDIUM,
-        ['font-normal']: fontStyle === FONT_STYLE.NORMAL,
-        ['italic']: fontStyle === FONT_STYLE.ITALIC,
-        ['text-lg']: fontSize === FONT_SIZE.LG,
-        ['text-sm']: fontSize === FONT_SIZE.SM,
-        ['text-xl']: fontSize === FONT_SIZE.XL,
-        ['text-md']: fontSize === FONT_SIZE.MD,
-        ['text-2xl']: fontSize === FONT_SIZE.XXL,
-        ['text-3xl']: fontSize === FONT_SIZE.XXXL,
-        ['text-4xl']: fontSize === FONT_SIZE.XXXXL,
-        ['font-sans']: fontFamily === FONT_FAMILY.SANS,
-        ['font-cinzel']: fontFamily === FONT_FAMILY.CINZEL,
-        ['font-cinzel-dec']: fontFamily === FONT_FAMILY.CINZEL_DEC,
+        ['font-bold']: font_style === FONT_STYLE.BOLD,
+        ['font-semibold']: font_style === FONT_STYLE.SEMI_BOLD,
+        ['font-medium']: font_style === FONT_STYLE.MEDIUM,
+        ['font-normal']: font_style === FONT_STYLE.NORMAL,
+        ['italic']: font_style === FONT_STYLE.ITALIC,
+        ['text-lg']: font_size === FONT_SIZE.LG,
+        ['text-sm']: font_size === FONT_SIZE.SM,
+        ['text-xl']: font_size === FONT_SIZE.XL,
+        ['text-md']: font_size === FONT_SIZE.MD,
+        ['text-2xl']: font_size === FONT_SIZE.XXL,
+        ['text-3xl']: font_size === FONT_SIZE.XXXL,
+        ['text-4xl']: font_size === FONT_SIZE.XXXXL,
+        ['font-sans']: font_family === FONT_FAMILY.SANS,
+        ['font-cinzel']: font_family === FONT_FAMILY.CINZEL,
+        ['font-cinzel-dec']: font_family === FONT_FAMILY.CINZEL_DEC,
         ['text-white']: color === COLORS.WHITE,
         ['text-black']: color === COLORS.BLACK
       })}
