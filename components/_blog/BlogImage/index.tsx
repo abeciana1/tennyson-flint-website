fffcc1dbc824eccd5f9ed40ba411a344f034c13f
abeciana1 <alex.beciana@gmail.com>
2025-02-02 16:48:57 -0700

    Adjusted props and deconstructed

--- components/_blog/BlogImage/index.tsx
+++ components/_blog/BlogImage/index.tsx
@@ -1,22 +1,25 @@
-import { ImageI } from '@/definitions/interfaces/_styled/ImageComp'
+import { BlogImageI } from '@/definitions/interfaces/_styled/ImageComp'
 import { ROUNDED } from '@/definitions/enums'
 import ImageComp from '@/components/_styled/ImageComp'
 
-const BlogImage: React.FC<ImageI> = ({
-  src,
-  alt,
-  width,
-  height,
-  rounded = ROUNDED.MD
+const BlogImage: React.FC<BlogImageI> = ({
+  blok
 }) => {
+  const {
+    file,
+    alt_text,
+    width,
+    height,
+    rounded_edges = ROUNDED.MD
+  } = blok
   return (
     <section className='px-5 sm:px-20 flex justify-center py-4 md:py-8'>
       <ImageComp
-        src={src}
-        alt={alt}
+        file={file}
+        alt_text={alt_text}
         width={width}
         height={height}
-        rounded={rounded}
+        rounded_edges={rounded_edges}
       />
     </section>
   )
