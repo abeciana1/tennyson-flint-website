7db63f8f588cea8dbc07a9090b1bfa849533f747
abeciana1 <alex.beciana@gmail.com>
2025-02-02 16:39:36 -0700

    Added meta data

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -11,33 +11,37 @@ import { ButtonLink } from '@/components/_styled/links'
 
 const NotFound = () => {
   return (
-    <main className='grow px-2.5'>
-      <section className='flex flex-col justify-center items-center text-center h-screen max-h-[500px]'>
-        <Heading1
-          text='Page Not Found'
-        />
-        <div className='mb-5'>
-          <TextContent
-            blok={{
-              text: 'The page you are looking for does not exist.',
-              font_size: FONT_SIZE.XXL,
-              color: COLORS.BLACK,
-              font_family: FONT_FAMILY.SANS
-            }}
+    <>
+      <title>Page Not Found | Tennyson Flint</title>
+      <meta name='description' content='The page you are looking for does not exist.' />
+      <main className='grow px-2.5'>
+        <section className='flex flex-col justify-center items-center text-center h-screen max-h-[500px]'>
+          <Heading1
+            text='Page Not Found'
           />
-        </div>
-        <ButtonLink
-          linkText='Return to the homepage'
-          href='/'
-          title='Return to the homepage'
-          bgColor={COLORS.NAVY}
-          textColor={COLORS.GOLD}
-          rounded={ROUNDED.MD}
-          fontStyle={FONT_STYLE.MEDIUM}
-          arrow
-        />
-      </section>
-    </main>
+          <div className='mb-5'>
+            <TextContent
+              blok={{
+                text: 'The page you are looking for does not exist.',
+                font_size: FONT_SIZE.XXL,
+                color: COLORS.BLACK,
+                font_family: FONT_FAMILY.SANS
+              }}
+            />
+          </div>
+          <ButtonLink
+            linkText='Return to the homepage'
+            href='/'
+            title='Return to the homepage'
+            bgColor={COLORS.NAVY}
+            textColor={COLORS.GOLD}
+            rounded={ROUNDED.MD}
+            fontStyle={FONT_STYLE.MEDIUM}
+            arrow
+          />
+        </section>
+      </main>
+    </>
   )
 }
 
