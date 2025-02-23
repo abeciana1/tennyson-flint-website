520b71a16318adc9567a7c0a10c19af7cae34cb7
abeciana1 <alex.beciana@gmail.com>
2025-02-23 16:38:14 -0700

    Removed wrapping marginsection and added justify center

--- components/_sections/BlogSection/index.tsx
+++ components/_sections/BlogSection/index.tsx
@@ -25,63 +25,61 @@ const BlogSection: React.FC<BlogSectionI> = ({
   } = blok
   const blogList = use(fetchContentStories('published', 'blog', { content_type: 'blogPage' }))
   return (
-    <section>
-      <MarginSection>
-        <Heading1
-          text={preheading}
-          fontSize='2xl'
-          fontFamily={FONT_FAMILY.SANS}
-        />
-        <TextContent
-          blok={{
-            text: headline,
-            data_test_id: 'headline',
-            font_size: FONT_SIZE.XXXXL,
-            font_style: FONT_STYLE.MEDIUM,
-            font_family: FONT_FAMILY.CINZEL
-          }}
-        />
-        {blogList?.data &&
-          <section className='mt-12 flex flex-row flex-wrap gap-6'>
-            {blogList?.data?.stories?.slice(0,3)?.map((blogPost: BlogPostI) => {
-              return (
-                <BlogPostCard
-                  key={blogPost?.uuid}
-                  title={blogPost?.name}
-                  excerpt={blogPost?.content?.excerpt}
-                  href={`/blog/${blogPost?.slug}`}
-                  image={{
-                    file: blogPost?.content?.featured_image[0]?.file,
-                    alt_text: `${blogPost?.name} featured blog image`,
-                    width: blogPost?.content?.featured_image[0]?.width,
-                    height: blogPost?.content?.featured_image[0]?.height,
-                    rounded_edges: ROUNDED.XL
-                  }}
-                  publishedDate={{
-                    month: format(new Date(blogPost?.first_published_at), "MMM"),
-                    day: format(new Date(blogPost?.first_published_at), "d")
-                  }}
-                  category={blogPost?.content?.category}
-                />
-              )
-            })}
-          </section>
-        }
-        {blogList && blogList?.data?.stories?.length > 2 &&
-          <div className='mt-12 flex justify-center'>
-            <ButtonLink
-              link_text='View all blogs'
-              title='Link to all blog posts pages'
-              href='/blog'
-              background_color={COLORS.NAVY}
-              text_color={COLORS.GOLD}
-              rounded_edges={ROUNDED.MD}
-              font_style={FONT_STYLE.BOLD}
-            />
-          </div>
-        }
-      </MarginSection>
-    </section>
+    <MarginSection>
+      <Heading1
+        text={preheading}
+        fontSize='2xl'
+        fontFamily={FONT_FAMILY.SANS}
+      />
+      <TextContent
+        blok={{
+          text: headline,
+          data_test_id: 'headline',
+          font_size: FONT_SIZE.XXXXL,
+          font_style: FONT_STYLE.MEDIUM,
+          font_family: FONT_FAMILY.CINZEL
+        }}
+      />
+      {blogList?.data &&
+        <section className='mt-12 flex flex-row flex-wrap gap-6 justify-center'>
+          {blogList?.data?.stories?.slice(0,3)?.map((blogPost: BlogPostI) => {
+            return (
+              <BlogPostCard
+                key={blogPost?.uuid}
+                title={blogPost?.name}
+                excerpt={blogPost?.content?.excerpt}
+                href={`/blog/${blogPost?.slug}`}
+                image={{
+                  file: blogPost?.content?.featured_image[0]?.file,
+                  alt_text: `${blogPost?.name} featured blog image`,
+                  width: blogPost?.content?.featured_image[0]?.width,
+                  height: blogPost?.content?.featured_image[0]?.height,
+                  rounded_edges: ROUNDED.XL
+                }}
+                publishedDate={{
+                  month: format(new Date(blogPost?.first_published_at), "MMM"),
+                  day: format(new Date(blogPost?.first_published_at), "d")
+                }}
+                category={blogPost?.content?.category}
+              />
+            )
+          })}
+        </section>
+      }
+      {blogList && blogList?.data?.stories?.length > 2 &&
+        <div className='mt-12 flex justify-center'>
+          <ButtonLink
+            link_text='View all blogs'
+            title='Link to all blog posts pages'
+            href='/blog'
+            background_color={COLORS.NAVY}
+            text_color={COLORS.GOLD}
+            rounded_edges={ROUNDED.MD}
+            font_style={FONT_STYLE.BOLD}
+          />
+        </div>
+      }
+    </MarginSection>
   )
 }
 
