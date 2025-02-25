4b93be23391559b2defc7ce69394cd1c28f772ac
abeciana1 <alex.beciana@gmail.com>
2025-02-25 16:34:44 -0700

    Reverted

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 export const runtime = "edge";
 
 const nextConfig: NextConfig = ({
-  experimental: {
-    dynamicIO: true,
-    cacheLife: {
-      "/": {
-        stale: 3600 * 4,
-        revalidate: 600,
-        expire: 86400
-      }
-    }
-  },
   images: {
     remotePatterns: [
       {
