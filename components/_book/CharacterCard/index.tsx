23ccdfea146eaa3dd4acf2ae91111f7847db13ac
abeciana1 <alex.beciana@gmail.com>
2025-02-25 18:41:00 -0700

    Added async fetch based on uuid

--- components/_book/CharacterCard/index.tsx
+++ components/_book/CharacterCard/index.tsx
@@ -1,4 +1,4 @@
-import { CharacterCardI } from '@/definitions/interfaces/_book'
+import { CharacterI } from '@/definitions/interfaces/_book'
 import TextContent from '@/components/_styled/Text'
 import ImageComp from '@/components/_styled/ImageComp'
 import {
@@ -6,19 +6,25 @@ import {
   FONT_STYLE,
   ROUNDED
 } from '@/definitions/enums'
+import { fetchSingleStory } from '@/helper-functions/storyblok-fetch'
+import { RoundedType } from '@/definitions/interfaces/_styled/ImageComp'
 
-const CharacterCard: React.FC<CharacterCardI> = ({
-  name,
-  description,
-  image
+const CharacterCard: React.FC<CharacterI> = async ({
+  uuid
 }) => {
+  const character = await fetchSingleStory(uuid, "published")
+  const {
+    name,
+    image,
+    description
+  } = await character?.story?.content
   const {
     file,
     alt_text,
     width,
     height,
     rounded_edges
-  } = image
+  } = image[0]
   return (
     <div className='flex gap-6 items-start lg:max-w-sm xl:max-w-md 2xl:max-w-lg relative mx-auto'>
       <div className='hidden sm:block'>
@@ -45,7 +51,7 @@ const CharacterCard: React.FC<CharacterCardI> = ({
             alt_text={alt_text}
             width={width}
             height={height}
-            rounded_edges={ROUNDED[rounded_edges]}
+            rounded_edges={ROUNDED[rounded_edges as RoundedType]}
           />
         </div>
         <div className='mt-6'>
