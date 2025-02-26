30d626d4a825348924a0cdc1717fa862e1170dfb
abeciana1 <alex.beciana@gmail.com>
2025-02-25 18:43:04 -0700

    Mapped over character uuid array

--- components/_book/MeetTheCharacters/index.tsx
+++ components/_book/MeetTheCharacters/index.tsx
@@ -1,8 +1,7 @@
 import MarginSection from '@/components/_sections/MarginSection'
 import { Heading2 } from '@/components/_styled/headings'
-import { MeetTheCharactersI, CharacterI } from '@/definitions/interfaces/_book'
+import { MeetTheCharactersI } from '@/definitions/interfaces/_book'
 import CharacterCard from '@/components/_book/CharacterCard'
-import { ROUNDED } from '@/definitions/enums'
 
 const MeetTheCharacters: React.FC<MeetTheCharactersI> = ({
   blok
@@ -19,19 +18,12 @@ const MeetTheCharacters: React.FC<MeetTheCharactersI> = ({
       {(characters && characters?.length > 0) &&
         <div className='flex flex-wrap gap-12 mt-6'>
           {characters?.map((
-            character: CharacterI) => {
+            character: string, index:number) => {
+              console.log('character', character)
             return (
               <CharacterCard
-                key={character?.content?._uid}
-                name={character?.content?.name}
-                description={character?.content?.description}
-                image={{
-                  file: character?.content?.image[0]?.file,
-                  alt_text: character?.content?.image[0]?.alt_text,
-                  width: character?.content?.image[0]?.width,
-                  height: character?.content?.image[0]?.height,
-                  rounded_edges: ROUNDED.MD
-                }}
+                key={`${character}-${index}`}
+                uuid={character}
               />
             )
           })}
