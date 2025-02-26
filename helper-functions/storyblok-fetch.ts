4f49d33259241d5c0e0bde26a05b980ba2cb1b4a
abeciana1 <alex.beciana@gmail.com>
2025-02-25 18:41:25 -0700

    Adjusted fetching urls

--- helper-functions/storyblok-fetch.ts
+++ helper-functions/storyblok-fetch.ts
@@ -1,18 +1,14 @@
-import {
-  getStoryblokApi,
-  StoryblokClient
-} from "@storyblok/react/rsc";
-
 export const fetchStory = async (
   version: "draft" | "published",
   slug?: string[]
 ) => {
   const correctSlug = `/${slug ? slug.join("/") : "home"}`;
-  const storyblokApi: StoryblokClient = getStoryblokApi();
-  return storyblokApi.get(`cdn/stories/${correctSlug}`,
-    { version: version,
-      resolve_relations: (correctSlug.indexOf('/books/') > -1) ? 'meetCharacters.characters' : '',
-    });
+  const resolveRelations = correctSlug.includes("/books/")
+  ? "meetCharacters.characters"
+  : "";
+  const res = await fetch(`https://api-us.storyblok.com/v2/cdn/stories${correctSlug}?cv=1740528764&token=${process.env.NEXT_PUBLIC_STORYBLOK_TOKEN}&version=${version}&resolve_relations=${resolveRelations}`)
+  const data = await res.json();
+  return data
 };
 
 export const fetchContentStories = async (
@@ -20,11 +16,42 @@ export const fetchContentStories = async (
   dirName: string,
   options = {}
 ) => {
-  const storyblokApi: StoryblokClient = getStoryblokApi();
-  const content = await storyblokApi?.get(`cdn/stories`, {
-    version: version,
-    starts_with: dirName,
-    ...options
-  })
-  return content
+  const params = new URLSearchParams({
+    ...Object.fromEntries(
+      Object.entries(options).map(([key, value]) => [key, String(value)])
+    ),
+  });
+
+
+  const url = `https://api-us.storyblok.com/v2/cdn/stories?starts_with=${dirName}${options && `&${params?.toString()}`}&token=${process.env.NEXT_PUBLIC_STORYBLOK_TOKEN}&version=${version}`
+
+  console.log('url', url)
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    next: { revalidate: 3600 },
+  });
+
+  const data = await res.json();
+  console.log('data', data);
+  return data
+}
+
+export const fetchSingleStory = async (uuid: string, version: "draft" | "published") => {
+  const url = `https://api-us.storyblok.com/v2/cdn/stories/${uuid}?find_by=uuid&token=${process.env.NEXT_PUBLIC_STORYBLOK_TOKEN}&version=${version}`
+  console.log('url', url)
+  const res = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+    },
+    next: { revalidate: 3600 },
+  });
+  const data = await res.json();
+  console.log('data', data);
+  return data
 }
